9d77a42aaba3e7798039778f43d1b189b2ce8d68
LLipter <katsudonn@163.com>
2018-11-30 18:55:06 +0800

    test usage for VGG19

--- style_transfer.py
+++ style_transfer.py
@@ -6,21 +6,41 @@ import time
 from keras.applications import vgg19
 from keras import backend as K
 
+img_nrows = 256
+img_ncols = 256
 
+# util function to open, resize and format pictures into appropriate tensors
+def preprocess_image(image_path):
+    img = load_img(image_path, target_size=(img_nrows, img_ncols))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = vgg19.preprocess_input(img)
+    return img
 
-# def preprocess_image(image_path):
-#     load_img()
-#     img = load_img(image_path, target_size=(img_nrows, img_ncols))
-#     img = img_to_array(img)
-#     img = np.expand_dims(img, axis=0)
-#     img = vgg19.preprocess_input(img)
-#     return img
+# util function to convert a tensor into a valid image
+def deprocess_image(x):
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape((3, img_nrows, img_ncols))
+        x = x.transpose((1, 2, 0))
+    else:
+        x = x.reshape((img_nrows, img_ncols, 3))
+    # Remove zero-center by mean pixel
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    # 'BGR'->'RGB'
+    x = x[:, :, ::-1]
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
 
 
 
 if __name__ == "__main__":
     print("hello world")
-    img = load_img("data/style/")
-    print(img)
-    print(img.size)
+    # img = preprocess_image("data/style/candy.jpg")
+    # print(img)
+    # print(img.shape)
+    input_tensor = K.placeholder((1, img_nrows, img_ncols, 3))
+    model = vgg19.VGG19(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
 
