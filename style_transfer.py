6b07c71d918ee3d81dfe5eba8a9d9f571be220ee
LLipter <katsudonn@163.com>
2018-11-30 20:27:37 +0800

    half of transfer model

--- style_transfer.py
+++ style_transfer.py
@@ -1,10 +1,12 @@
 from keras.preprocessing.image import load_img, save_img, img_to_array
 import numpy as np
-from scipy.optimize import fmin_l_bfgs_b
-import time
 
 from keras.applications import vgg19
 from keras import backend as K
+from keras.layers import Input, Conv2D, ReLU
+from keras.models import Model
+from keras.utils import plot_model
+from keras_contrib.layers import InstanceNormalization
 
 img_nrows = 256
 img_ncols = 256
@@ -34,13 +36,52 @@ def deprocess_image(x):
     return x
 
 
+def downSampling(x, filters, kernel_size, strides, padding):
+    conv1 = Conv2D(filters=filters,
+                   kernel_size=kernel_size,
+                   strides=strides,
+                   padding=padding,
+                   data_format=K.image_data_format())(x)
+    if K.image_data_format() == 'channels_first':
+        instance_normalized = InstanceNormalization(axis=1)(conv1)
+    else:
+        instance_normalized = InstanceNormalization(axis=3)(conv1)
+    relu = ReLU()(instance_normalized)
+    return relu
+
+
+def transform_model():
+    if K.image_data_format() == 'channels_first':
+        input = Input(shape=(3, img_nrows, img_ncols))
+    else:
+        input = Input(shape=(img_nrows, img_ncols, 3))
+
+    conv1 = downSampling(input, 32, 9, 1, "same")
+    conv2 = downSampling(conv1, 64, 3, 2, "valid")
+    conv3 = downSampling(conv2, 128, 3, 2, "valid")
+
+    transformNet = Model(inputs=input, outputs=conv3)
+    plot_model(transformNet, to_file="img/model/transform.png", show_shapes=True)
+    return transformNet
+
+
+
 
 if __name__ == "__main__":
     print("hello world")
     # img = preprocess_image("img/style/candy.jpg")
     # print(img)
     # print(img.shape)
-    input_tensor = K.placeholder((1, img_nrows, img_ncols, 3))
-    model = vgg19.VGG19(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
+    # input_tensor = K.placeholder((1, img_nrows, img_ncols, 3))
+    # model = vgg19.VGG19(input_tensor=input_tensor, weights='imagenet', include_top=False)
+    # print('Model loaded.')
+    # outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
+    # for key in outputs_dict.keys():
+    #     print(key)
+
+    transformNet = transform_model()
+    print(K.image_data_format())
+
 
 
