dc876c4ef446a1f08ea3e6ee2673119e50f29597
LLipter <katsudonn@163.com>
2018-11-30 15:13:56 +0800

    add style and test img

--- style_transfer.py
+++ style_transfer.py
@@ -7,5 +7,20 @@ from keras.applications import vgg19
 from keras import backend as K
 
 
+
+# def preprocess_image(image_path):
+#     load_img()
+#     img = load_img(image_path, target_size=(img_nrows, img_ncols))
+#     img = img_to_array(img)
+#     img = np.expand_dims(img, axis=0)
+#     img = vgg19.preprocess_input(img)
+#     return img
+
+
+
 if __name__ == "__main__":
     print("hello world")
+    img = load_img("data/style/")
+    print(img)
+    print(img.size)
+
