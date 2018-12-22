a3163187ea22b076d62200b54836a943e1146c67
LLipter <katsudonn@163.com>
2018-12-23 04:51:48 +0800

    Refactor

---
+++ ffmpeg/io.h
@@ -0,0 +1,14 @@
+#ifndef FFMPEG_IO_H
+#define FFMPEG_IO_H
+
+#include <dirent.h>
+#include <sys/stat.h>
+#include <zconf.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int remove_directory(const char *path);
+
+#endif //FFMPEG_IO_H
