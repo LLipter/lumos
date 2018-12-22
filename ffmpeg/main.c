417d0583363376f5322fd4a53c04253ca6c72d20
LLipter <katsudonn@163.com>
2018-12-22 23:38:52 +0800

    1

--- ffmpeg/main.c
+++ ffmpeg/main.c
@@ -8,14 +8,20 @@
 char* input_path = "../video/01.mp4";
 AVFormatContext *pFormatCtx = NULL;
 int video_stream_index = -1;
+AVCodecParameters *pCodePara = NULL;
+AVCodec *pCodec = NULL;
+AVCodecContext *codec_ctx = NULL;
+
 
 void cleanup(char* msg){
-    if(pFormatCtx){
+    if(pFormatCtx)
         avformat_free_context(pFormatCtx);
-        avformat_close_input(&pFormatCtx);
+    if(codec_ctx)
+        avcodec_free_context(codec_ctx);
+    if(msg){
+        perror(msg);
+        exit(1);
     }
-    perror(msg);
-    exit(1);
 }
 
 int main(int argc, char **argv) {
@@ -39,7 +45,24 @@ int main(int argc, char **argv) {
     if (video_stream_index == -1)
         cleanup("Cannot find stream index");
 
+    //5、获取解码器
+    pCodePara = pFormatCtx->streams[video_stream_index]->codecpar;
+    int width = pCodePara->width;
+    int height = pCodePara->height;
+    pCodec = avcodec_find_decoder(pCodePara->codec_id);
+    if (pCodec == NULL)
+        cleanup("Cannot find decoder");
+
+    //6、打开解码器
+//    codec_ctx = avcodec_alloc_context3(pCodec);
+//    avcodec_parameters_to_context(codec_ctx, pCodePara);
+    if (avcodec_open2(pCodePara, pCodec, NULL) < 0)
+        cleanup("Cannot open codec");
 
 
+
+
+
+    cleanup(NULL);
     return 0;
 }
