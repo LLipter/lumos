f84a57af41554a5d22b89c539218248a0c3386b6
LLipter <katsudonn@163.com>
2018-12-23 03:58:35 +0800

    sss

--- ffmpeg/main.c
+++ ffmpeg/main.c
@@ -4,6 +4,8 @@
 
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
 
 #include <dirent.h>
 #include <sys/stat.h>
@@ -13,7 +15,6 @@
 
 char *input_dir = "../video/%s";
 char *filename = "01.mp4";
-char *save_dir = "../video/tmp/%s";
 char save_path[BUFF_SIZE];
 char input_path[BUFF_SIZE];
 char buf[BUFF_SIZE];
@@ -27,6 +28,9 @@ AVCodecContext *codec_ctx = NULL;
 AVCodecParameters *pCodePara = NULL;
 AVPacket *packet = NULL;
 AVFrame *frame = NULL;
+struct SwsContext *sws_ctx = NULL;
+AVFrame *yuvFrame = NULL;
+uint8_t *out_buffer = NULL;
 
 int remove_directory(const char *path);
 void pgm_save(unsigned char *buf, int wrap, char *filename);
@@ -41,6 +45,10 @@ void cleanup(char *msg) {
         av_packet_unref(packet);
     if (frame)
         av_frame_free(&frame);
+    if (yuvFrame)
+        av_frame_free(&yuvFrame);
+    if (out_buffer)
+        av_free(out_buffer);
     if (msg) {
         perror(msg);
         exit(1);
@@ -50,17 +58,19 @@ void cleanup(char *msg) {
 
 int extract_frame(AVFrame *frame) {
     if (frame->pict_type == AV_PICTURE_TYPE_I) {
-//        if (got_picture_ptr) {
-//            //frame->yuvFrame，转为指定的YUV420P像素帧
-//            sws_scale(sws_ctx, (const uint8_t *const *) frame->data, frame->linesize, 0,
-//                      frame->height, yuvFrame->data, yuvFrame->linesize);
-//            //计算视频数据总大小
-//            int y_size = pCodeCtx->width * pCodeCtx->height;
-//            //AVFrame->YUV，由于YUV的比例是4:1:1
-//            fwrite(yuvFrame->data[0], 1, y_size, fp_yuv);
-//            fwrite(yuvFrame->data[1], 1, y_size / 4, fp_yuv);
-//            fwrite(yuvFrame->data[2], 1, y_size / 4, fp_yuv);
-//        }
+        snprintf(buf, BUFF_SIZE, "%s/%s-%d.yuv", save_path, filename, codec_ctx->frame_number);
+//        pgm_save(frame->data[0], frame->linesize[0], buf);
+        FILE* fp_yuv = fopen(buf, "w");
+        //frame->yuvFrame，转为指定的YUV420P像素帧
+        sws_scale(sws_ctx, (const uint8_t *const *) frame->data, frame->linesize, 0,
+                  frame->height, yuvFrame->data, yuvFrame->linesize);
+        //计算视频数据总大小
+        int y_size = width * height;
+        //AVFrame->YUV，由于YUV的比例是4:1:1
+        fwrite(yuvFrame->data[0], 1, y_size, fp_yuv);
+        fwrite(yuvFrame->data[1], 1, y_size / 4, fp_yuv);
+        fwrite(yuvFrame->data[2], 1, y_size / 4, fp_yuv);
+
     }
     return 0;
 }
@@ -68,8 +78,9 @@ int extract_frame(AVFrame *frame) {
 void init() {
     if (snprintf(input_path, BUFF_SIZE, input_dir, filename) < 0)
         cleanup("Error in snprintf");
-    if (snprintf(save_path, BUFF_SIZE, save_dir, filename) < 0)
+    if (snprintf(save_path, BUFF_SIZE, "%s-IFrame", input_path) < 0)
         cleanup("Error in snprintf");
+    remove_directory(save_path);
     if (mkdir(save_path, 0777) < 0)
         cleanup("Error in mkdir");
 }
@@ -105,8 +116,20 @@ int main(int argc, char **argv) {
     if (avcodec_open2(codec_ctx, pCodec, NULL) < 0)
         cleanup("Error in opening codec");
 
+    // change format
+    yuvFrame = av_frame_alloc();
+    sws_ctx = sws_getContext(
+            width,
+            height,
+            codec_ctx->pix_fmt,
+            width,
+            height,
+            AV_PIX_FMT_YUV420P, SWS_BILINEAR, NULL, NULL, NULL);
+    out_buffer = (uint8_t *) av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P, width, height, 1));
+    av_image_fill_arrays(yuvFrame->data, yuvFrame->linesize, out_buffer, AV_PIX_FMT_YUV420P, width, height, 1);
+
+
     //7、解析每一帧数据
-    int got_picture_ptr, frame_count = 1;
     packet = av_packet_alloc();
     frame = av_frame_alloc();
 
@@ -126,7 +149,7 @@ int main(int argc, char **argv) {
         }
     }
     // send flush packet, enter draining mode.
-    avcodec_send_packet(NULL, NULL);
+    avcodec_send_packet(codec_ctx, NULL);
     while ((ret = avcodec_receive_frame(codec_ctx, frame)) >= 0)
         extract_frame(frame);
     if (ret != AVERROR_EOF)
