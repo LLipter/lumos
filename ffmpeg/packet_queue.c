0764cacc23e0c07972ee836c975723bb0fccdc63
LLipter <katsudonn@163.com>
2018-12-25 03:54:52 +0800

    1

--- ffmpeg/packet_queue.c
+++ ffmpeg/packet_queue.c
@@ -4,37 +4,33 @@
 
 #include "packet_queue.h"
 
-int full_size;
-int cur_size;
-int head_pos;
-AVPacket **packet_array;
 
-void packet_queue_alloc(int size) {
-    full_size = size;
-    cur_size = 0;
-    head_pos = 0;
-    packet_array = malloc(sizeof(AVPacket *) * size);
+void packet_queue_alloc(Packet_Queue *packet_queue, int size) {
+    packet_queue->full_size = size;
+    packet_queue->cur_size = 0;
+    packet_queue->head_pos = 0;
+    packet_queue->packet_array = malloc(sizeof(AVPacket *) * size);
 }
 
-void packet_queue_free(){
-    free(packet_array);
+void packet_queue_free(Packet_Queue *packet_queue) {
+    free(packet_queue->packet_array);
 }
 
-int push_packet(AVPacket *packet){
-    if(cur_size == full_size)
+int push_packet(Packet_Queue *packet_queue, AVPacket *packet) {
+    if (packet_queue->cur_size == packet_queue->full_size)
         return -1;
-    int pos = (head_pos + cur_size) % full_size;
-    packet_array[pos] = packet;
-    cur_size++;
+    int pos = (packet_queue->head_pos + packet_queue->cur_size) % packet_queue->full_size;
+    packet_queue->packet_array[pos] = packet;
+    packet_queue->cur_size++;
     return 0;
 }
 
-AVPacket * pop_packet(){
-    if(cur_size == 0)
+AVPacket *pop_packet(Packet_Queue *packet_queue) {
+    if (packet_queue->cur_size == 0)
         return NULL;
-    AVPacket * packet = packet_array[head_pos];
-    head_pos = (head_pos + 1) % full_size;
-    cur_size--;
+    AVPacket *packet = packet_queue->packet_array[packet_queue->head_pos];
+    packet_queue->head_pos = (packet_queue->head_pos + 1) % packet_queue->full_size;
+    packet_queue->cur_size--;
     return packet;
 }
 
