51571a245eb87e485127a9f97d15fa4d2d0281e6
尹家桐 <1132735216@qq.com>
2018-12-25 00:06:00 +0800

    main function

--- ffmpeg/main.c
+++ ffmpeg/main.c
@@ -16,27 +16,39 @@ char *input_dir = "../video";
 char *filename = "02.mp4";
 char save_path[BUFF_SIZE];
 char input_path[BUFF_SIZE];
+char output_path[BUFF_SIZE];
 char buf[BUFF_SIZE];
 int width = 0;
 int height = 0;
 int ret = 0;
-int video_stream_index = -1;
+int i_video_stream_index = -1;
+int o_video_stream_index = -1;
 int frame_cnt = 1;
-AVFormatContext *pFormatCtx = NULL;
-AVCodec *pCodec = NULL;
-AVCodecContext *codec_ctx = NULL;
-AVCodecParameters *pCodePara = NULL;
-AVPacket *packet = NULL;
+AVFormatContext *ifmt_ctx = NULL;
+AVFormatContext *ofmt_ctx = NULL;
+AVCodec *i_pCodec = NULL;
+AVCodec *o_pCodec = NULL;
+AVCodecContext *i_codec_ctx = NULL;
+AVCodecContext *o_codec_ctx = NULL;
+AVCodecParameters *i_pCodePara = NULL;
+AVCodecParameters *o_pCodePara = NULL;
+AVPacket *p_packet = NULL;
 AVFrame *frame = NULL;
+AVOutputFormat *ofmt = NULL;
+int i;
 
 
 void cleanup(char *msg) {
-    if (pFormatCtx)
-        avformat_close_input(&pFormatCtx);
-    if (codec_ctx)
-        avcodec_free_context(&codec_ctx);
-    if (packet)
-        av_packet_unref(packet);
+    if (ifmt_ctx)
+        avformat_close_input(&ifmt_ctx);
+    if (ofmt_ctx)
+        avformat_close_input(&ofmt_ctx);
+    if (i_codec_ctx)
+        avcodec_free_context(&i_codec_ctx);
+    if (o_codec_ctx)
+        avcodec_free_context(&o_codec_ctx);
+    if (p_packet)
+        av_packet_unref(p_packet);
     if (frame)
         av_frame_free(&frame);
     if (msg) {
@@ -50,6 +62,8 @@ void init() {
         cleanup("Error in snprintf");
     if (snprintf(save_path, BUFF_SIZE, "%s-IFrame", input_path) < 0)
         cleanup("Error in snprintf");
+    if (snprintf(output_path, BUFF_SIZE, "%s/out-%s", input_dir, filename) < 0)
+        cleanup("Error in snprintf");
 }
 
 void save_frame_as_jpeg(AVFrame *pFrame, char *filename) {
@@ -59,10 +73,10 @@ void save_frame_as_jpeg(AVFrame *pFrame, char *filename) {
     AVCodecContext *jpegContext = avcodec_alloc_context3(jpegCodec);
     if (!jpegContext)
         cleanup("Error in create jpeg context");
-    jpegContext->pix_fmt = codec_ctx->pix_fmt;
+    jpegContext->pix_fmt = i_codec_ctx->pix_fmt;
     jpegContext->height = height;
     jpegContext->width = width;
-    jpegContext->time_base = codec_ctx->time_base;
+    jpegContext->time_base = i_codec_ctx->time_base;
     if (avcodec_open2(jpegContext, jpegCodec, NULL) < 0)
         cleanup("Error in opening jpeg decoder");
     AVPacket *jpeg_packet = av_packet_alloc();
@@ -132,28 +146,203 @@ void extract_frame(AVFrame *frame) {
     printf("%d\n", frame_cnt);
     if (frame->pict_type == AV_PICTURE_TYPE_I) {
         snprintf(buf, BUFF_SIZE, "%s/%s-%d.jpg", save_path, filename, frame_cnt);
-        if (strcmp(cmd, "split") == 0)
-            save_frame_as_jpeg(frame, buf);
-        else
-            load_jpeg_as_frame(frame, buf);
+        save_frame_as_jpeg(frame, buf);
+
     }
     frame_cnt++;
 
-    if (strcmp(cmd, "split") != 0) {
-
-
-        /*
-         *
-         * 这他妈也是要写但还没写的。
-         * 在第二轮融合的时候，我们需要把每一帧重新写入输出文件中，
-         * https://stackoverflow.com/questions/40800489/ffmpeg-read-frame-process-it-put-it-to-output-video-copy-sound-stream-unchan
-         * 参考这个文章
-         *
-         * 我不干了
-         */
-    }
 }
 
+int split() {
+    // remove old data
+    remove_directory(save_path);
+    if (mkdir(save_path, 0777) < 0)
+        cleanup("Error in mkdir");
+
+
+    //2、打开视频文件
+    ifmt_ctx = avformat_alloc_context();
+    if ((avformat_open_input(&ifmt_ctx, input_path, NULL, NULL)) < 0)
+        cleanup("Error in opening input file");
+
+    //3、获取视频信息
+    if (avformat_find_stream_info(ifmt_ctx, NULL) < 0)
+        cleanup("Error in finding stream");
+
+    //4、找到视频流的位置
+    i_video_stream_index = av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &i_pCodec, 0);
+    if (i_video_stream_index == AVERROR_STREAM_NOT_FOUND)
+        cleanup("Error in finding stream index");
+    else if (i_video_stream_index == AVERROR_DECODER_NOT_FOUND)
+        cleanup("Error in finding decoder");
+
+    //5、获取AVCodecParameters
+    i_pCodePara = ifmt_ctx->streams[i_video_stream_index]->codecpar;
+    width = i_pCodePara->width;
+    height = i_pCodePara->height;
+
+    //6、打开解码器
+    i_codec_ctx = avcodec_alloc_context3(i_pCodec);
+    avcodec_parameters_to_context(i_codec_ctx, i_pCodePara);
+    if (avcodec_open2(i_codec_ctx, i_pCodec, NULL) < 0)
+        cleanup("Error in opening codec");
+
+    //7、解析每一帧数据
+    p_packet = av_packet_alloc();
+    frame = av_frame_alloc();
+    // read all frames and send them into decoder
+
+    while (av_read_frame(ifmt_ctx, p_packet) >= 0) {
+        if (p_packet->stream_index == i_video_stream_index) {
+            if (avcodec_send_packet(i_codec_ctx, p_packet) < 0)
+                cleanup("Error in sending a packet for decoding");
+            while ((ret = avcodec_receive_frame(i_codec_ctx, frame)) >= 0)
+                extract_frame(frame);
+            if (ret == AVERROR(EAGAIN))
+                continue;
+            else if (ret < 0)
+                cleanup("Error in receiving a packet from decoder");
+        }
+    }
+    // send flush packet, enter draining mode.
+    avcodec_send_packet(i_codec_ctx, NULL);
+    while ((ret = avcodec_receive_frame(i_codec_ctx, frame)) >= 0)
+        extract_frame(frame);
+    if (ret != AVERROR_EOF)
+        cleanup("Error in draining decoder stream");
+
+    cleanup(NULL);
+    return 1;
+}
+
+int merge() {
+    if (avformat_open_input(&ifmt_ctx, input_path, 0, 0) < 0)
+        cleanup("Could not open input file");
+
+    // Extract streams description
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0)
+        cleanup("Failed to retrieve input stream information");
+
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, output_path);
+    if (!ofmt_ctx) {
+        cleanup("Could not create output context\n");
+    }
+    ofmt = ofmt_ctx->oformat;
+
+    // Allocating output streams
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+        if (!out_stream) {
+            cleanup("Failed allocating output stream\n");
+        }
+        ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
+        if (ret < 0) {
+            cleanup("Failed to copy context from input to output stream codec context\n");
+        }
+        out_stream->codec->codec_tag = 0;
+        if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER) {
+            out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+        }
+    }
+
+    // Open output file
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, output_path, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            cleanup("Could not open output file");
+        }
+    }
+
+    //header
+    ret = avformat_write_header(ofmt_ctx, NULL);
+    if (ret < 0) {
+        cleanup("Error in writing header\n");
+    }
+
+    // input codec
+    i_video_stream_index = av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &i_pCodec, 0);
+    if (i_video_stream_index == AVERROR_STREAM_NOT_FOUND)
+        cleanup("Error in finding stream index");
+    else if (i_video_stream_index == AVERROR_DECODER_NOT_FOUND)
+        cleanup("Error in finding decoder");
+
+    i_pCodePara = ifmt_ctx->streams[i_video_stream_index]->codecpar;
+    width = i_pCodePara->width;
+    height = i_pCodePara->height;
+
+    i_codec_ctx = avcodec_alloc_context3(i_pCodec);
+    avcodec_parameters_to_context(i_codec_ctx, i_pCodePara);
+    if (avcodec_open2(i_codec_ctx, i_pCodec, NULL) < 0)
+        cleanup("Error in opening codec");
+
+    // output codec
+    o_video_stream_index = av_find_best_stream(ofmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &o_pCodec, 0);
+    if (o_video_stream_index == AVERROR_STREAM_NOT_FOUND)
+        cleanup("Error in finding stream index");
+    else if (o_video_stream_index == AVERROR_DECODER_NOT_FOUND)
+        cleanup("Error in finding decoder");
+
+    o_pCodePara = ofmt_ctx->streams[o_video_stream_index]->codecpar;
+
+    o_codec_ctx = avcodec_alloc_context3(o_pCodec);
+    avcodec_parameters_to_context(o_codec_ctx, o_pCodePara);
+    if (avcodec_open2(o_codec_ctx, o_pCodec, NULL) < 0)
+        cleanup("Error in opening codec");
+
+
+
+    frame = av_frame_alloc();
+    while (1) {
+        AVPacket packet;
+        ret = av_read_frame(ifmt_ctx, &packet);
+        if (ret < 0)
+            break;
+
+        AVStream *in_stream, *out_stream;
+        in_stream = ifmt_ctx->streams[packet.stream_index];
+        out_stream = ofmt_ctx->streams[packet.stream_index];
+
+        // copy packet
+        packet.pts = av_rescale_q_rnd(packet.pts, in_stream->time_base, out_stream->time_base,
+                                      AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX);
+        packet.dts = av_rescale_q_rnd(packet.dts, in_stream->time_base, out_stream->time_base,
+                                      AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX);
+        packet.duration = av_rescale_q(packet.duration, in_stream->time_base, out_stream->time_base);
+
+        if (packet.stream_index == i_video_stream_index) {
+            // 1. input_video_packet -> input_frame
+            // 2. is input_frame.type == I_FRAME goto 4
+            // 3. throw input_video_packet to av_interleaved_write_frame
+            // 4. read jpeg file from disk into a jpeg_packet
+            // *** need to create a new jpeg codec
+            // 5. decode jpeg_packet into new_input_frame
+            // *** need to create a new video codec, mustn't use defined o_codec_ctx
+            // 6. encode new_input_frame into output_video_packet
+            // 7. throw output_video_packet into output_stream
+
+
+
+            av_packet_unref(&packet);
+        } else {
+            if (av_interleaved_write_frame(ofmt_ctx, &packet) < 0)
+                cleanup("error in writing other stream");
+        }
+
+    }
+
+
+    if(av_interleaved_write_frame(ofmt_ctx, NULL) < 0)
+        cleanup("error in flush");
+
+    if ((av_write_trailer(ofmt_ctx) < 0))
+        cleanup("error in write trailer");
+
+
+    cleanup(NULL);
+
+
+}
 
 int main(int argc, char **argv) {
     if (argc <= 3) {
@@ -167,64 +356,14 @@ int main(int argc, char **argv) {
     init();
 
     if (strcmp(cmd, "split") == 0) {
-        // remove old data
-        remove_directory(save_path);
-        if (mkdir(save_path, 0777) < 0)
-            cleanup("Error in mkdir");
+        split();
+
+    } else {
+        merge();
     }
-
-    //2、打开视频文件
-    pFormatCtx = avformat_alloc_context();
-    if ((avformat_open_input(&pFormatCtx, input_path, NULL, NULL)) < 0)
-        cleanup("Error in opening input file");
-
-    //3、获取视频信息
-    if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
-        cleanup("Error in finding stream");
-
-    //4、找到视频流的位置
-    video_stream_index = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, &pCodec, 0);
-    if (video_stream_index == AVERROR_STREAM_NOT_FOUND)
-        cleanup("Error in finding stream index");
-    else if (video_stream_index == AVERROR_DECODER_NOT_FOUND)
-        cleanup("Error in finding decoder");
-
-    //5、获取AVCodecParameters
-    pCodePara = pFormatCtx->streams[video_stream_index]->codecpar;
-    width = pCodePara->width;
-    height = pCodePara->height;
-
-    //6、打开解码器
-    codec_ctx = avcodec_alloc_context3(pCodec);
-    avcodec_parameters_to_context(codec_ctx, pCodePara);
-    if (avcodec_open2(codec_ctx, pCodec, NULL) < 0)
-        cleanup("Error in opening codec");
-
-    //7、解析每一帧数据
-    packet = av_packet_alloc();
-    frame = av_frame_alloc();
-    // read all frames and send them into decoder
-
-    while (av_read_frame(pFormatCtx, packet) >= 0) {
-        if (packet->stream_index == video_stream_index) {
-            if (avcodec_send_packet(codec_ctx, packet) < 0)
-                cleanup("Error in sending a packet for decoding");
-            while ((ret = avcodec_receive_frame(codec_ctx, frame)) >= 0)
-                extract_frame(frame);
-            if (ret == AVERROR(EAGAIN))
-                continue;
-            else if (ret < 0)
-                cleanup("Error in receiving a packet from decoder");
-        }
-    }
-    // send flush packet, enter draining mode.
-    avcodec_send_packet(codec_ctx, NULL);
-    while ((ret = avcodec_receive_frame(codec_ctx, frame)) >= 0)
-        extract_frame(frame);
-    if (ret != AVERROR_EOF)
-        cleanup("Error in draining decoder stream");
-
-    cleanup(NULL);
 }
+
+
+
 
 
